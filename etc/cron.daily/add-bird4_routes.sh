de94d9c967685efd1ca198499a894d531e8fc692
DennoN-RUS <dennon.ru@gmail.com>
2020-05-24 19:39:55 +0300

    Update add-bird4_routes.sh

--- etc/cron.daily/add-bird4_routes.sh
+++ etc/cron.daily/add-bird4_routes.sh
@@ -3,11 +3,11 @@
 ISP=eth3
 VPN=ppp0
 URL0=https://antifilter.download/list/allyouneed.lst
+
 ROUTE=/opt/etc/bird4-routes.list
 VPNTXT=/opt/etc/bird4-vpn.txt
 ISPTXT=/opt/etc/bird4-isp.txt
 
-rm $ROUTE
 curl $URL0 | sed 's/^/route /' | sed  's/$/ via "'$VPN'";/' > $ROUTE
 /opt/root/addip.sh $VPNTXT $VPN $ROUTE
 /opt/root/addip.sh $ISPTXT $ISP $ROUTE

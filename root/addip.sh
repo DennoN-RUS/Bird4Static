55143475f151a68f01b679cd03927eaf7e5c5c81
DennoN-RUS <dennon.ru@gmail.com>
2020-05-24 15:29:48 +0300

    Update addip.sh

--- root/addip.sh
+++ root/addip.sh
@@ -4,7 +4,7 @@ cut_local() {
 	grep -vE 'localhost|^0\.|^127\.|^10\.|^172\.16\.|^192\.168\.|^::|^fc..:|^fd..:|^fe..:'
 }
 
-cat $1 | awk '{print $1}' | awk '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/' | sed 's/^/route /' | sed  's/$/\/32 via "'$2'";/' > $3
+cat $1 | awk '{print $1}' | awk '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/' | sed 's/^/route /' | sed  's/$/\/32 via "'$2'";/' >> $3
 cat $1 | awk '{print $1}' | awk '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/' | sed 's/^/route /' | sed  's/$/ via "'$2'";/' >> $3
 
 until ADDRS=$(dig +short google.com @localhost -p 53) && [ -n "$ADDRS" ] > /dev/null 2>&1; do sleep 5; done

8d430d4540b4c73975ea2962758b5fd4aed6c5cd
DennoN-RUS <dennon.ru@gmail.com>
2020-05-29 13:22:54 +0300

    Update 010-add_antizapret_route.sh

--- etc/ndm/ifstatechanged.d/010-add_antizapret_route.sh
+++ etc/ndm/ifstatechanged.d/010-add_antizapret_route.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 [ "$1" == "hook" ] || exit
-[ "$id" == "L2TP0" ] || exit
+[ "$id" == "IKE0" ] || exit
 
 case ${change}-${connected}-${link}-${up} in
 	link-no-down-down)
-		ip rule del table 1000
 	/opt/etc/init.d/S04bird1-ipv4 stop
 	;;
 	link-yes-up-up)
-	if [ -z "$(ip rule | grep 1000)" ]; then
-	        ip rule add table 1000
+	if [ -z "$(ip rule | awk '/^2150/' )" ]; then
+		ip rule add table 1000 priority 2150
 	fi
+	/opt/etc/cron.daily/add-bird4_routes.sh
 	/opt/etc/init.d/S04bird1-ipv4 start
 	;;
 esac

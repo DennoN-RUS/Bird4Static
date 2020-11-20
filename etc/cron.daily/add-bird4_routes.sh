c4718f52f1125f471bab9048110c6836f506d16f
Max <ienergystaff@gmail.com>
2020-11-21 00:09:58 +0300

    Update add-bird4_routes.sh

--- etc/cron.daily/add-bird4_routes.sh
+++ etc/cron.daily/add-bird4_routes.sh
@@ -8,6 +8,8 @@ ROUTE=/opt/etc/bird4-routes.list
 VPNTXT=/opt/etc/bird4-vpn.txt
 ISPTXT=/opt/etc/bird4-isp.txt
 
+[ -f "$ROUTE" ] && cat /dev/null > $ROUTE
+
 /opt/root/addip.sh $VPNTXT $VPN $ROUTE
 /opt/root/addip.sh $ISPTXT $ISP $ROUTE
 curl -sf $URL0 | sed 's/^/route /' | sed  's/$/ via "'$VPN'";/' >> $ROUTE

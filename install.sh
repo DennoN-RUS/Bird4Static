d796799014dcd3a1e8f2b779d89857d4087edea3
Max <ienergystaff@gmail.com>
2020-11-20 23:05:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,12 +14,12 @@ ifconfig | grep -B 1 "inet addr" | awk '{print $1$2}' | sed ':a;N;$!ba;s/Link\n/
 echo "Введите имя интерфейса провайдера из списка выше (например, ppp0 или eth3)"
 read ISP
 sed -i 's/eth3/'$ISP'/' /opt/etc/cron.daily/add-bird4_routes.sh
-echo "Введите имя интерфейса VPN из списка выше (например, ppp1 или nwg0)"
+echo "Введите имя интерфейса VPN из списка выше (например, ovpn_br0 или nwg0)"
 read VPN
 sed -i 's/ppp0/'$VPN'/' /opt/etc/cron.daily/add-bird4_routes.sh
 
 ndmc -c "show interface" | awk '/Inter/ || /addr/' | grep -B 1 "addr" | awk '!/--/' | awk '{print $4$2}' | sed ':a;N;$!ba;s/\"name\n/ <-- /g;s/\"//g'
-echo "Введите имя интерфейса VPN из списка выше (например, IKE0 или L2TP0)"
+echo "Введите имя интерфейса VPN из списка выше (например, OpenVPN0 или Wireguard0)"
 read VPNC
 sed -i 's/L2TP0/'$VPNC'/' /opt/etc/ndm/ifstatechanged.d/010-add_antizapret_route.sh
 

c2882d43c8f4a31ada394a8002ee95049437eee5
DennoN-RUS <dennon.ru@gmail.com>
2021-11-22 22:51:43 +0300

    fix env

--- Install/add-bird4_routes.sh
+++ Install/add-bird4_routes.sh
@@ -5,13 +5,15 @@ VPN=VPNINPUT
 URL0=https://antifilter.download/list/allyouneed.lst
 #URL0=https://antifilter.download/list/ip.lst
 
+HOMEPATH=/opt/root/Bird4Static
+
 ROUTE=/opt/etc/bird4-routes.list
-VPNTXT=$HOME/Bird4Static/lists/user-vpn.list
-ISPTXT=$HOME/Bird4Static/lists/user-isp.list
+VPNTXT=$HOMEPATH/lists/user-vpn.list
+ISPTXT=$HOMEPATH/lists/user-isp.list
 
 #curl $URL0 | sed 's/^/route /' | sed  's/$/\/32 via "'$VPN'";/' > $ROUTE
 curl $URL0 | sed 's/^/route /' | sed  's/$/ via "'$VPN'";/' > $ROUTE
-$HOME/Bird4Static/scripts/addip.sh $VPNTXT $VPN $ROUTE
-$HOME/Bird4Static/scripts/addip.sh $ISPTXT $ISP $ROUTE
+$HOMEPATH/scripts/addip.sh $VPNTXT $VPN $ROUTE
+$HOMEPATH/scripts/addip.sh $ISPTXT $ISP $ROUTE
 
 killall -s SIGHUP bird4

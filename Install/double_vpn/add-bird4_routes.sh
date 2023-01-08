cc7c300175dbb7d46348dcfa72debcabde216afb
DennoN-RUS <dennon.ru@gmail.com>
2023-01-08 22:05:39 +0300

    v3.3.2

--- Install/double_vpn/add-bird4_routes.sh
+++ Install/double_vpn/add-bird4_routes.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
  #USER VARIABLE
+DEBUG=0
 ISP=ISPINPUT
 VPN1=VPN1INPUT
 VPN2=VPN2INPUT
@@ -37,14 +38,28 @@ fi
 
  #IPRANGE FUNCTION
 ipr_func() {
+  if [[ "$DEBUG" == 1 ]]; then ipr_verb="-v"; fi
   if [[ $1 =~ ^\([0-9]{1,3}\.\){3}[0-9]{1,3}$ ]]; then
-    get_as_func "$2" | iprange --print-prefix "route " --print-suffix-nets " via $1;" --print-suffix-ips "/32 via $1;" -
+    get_as_func "$2" | iprange $ipr_verb --print-prefix "route " --print-suffix-nets " via $1;" --print-suffix-ips "/32 via $1;" -
   else
-    get_as_func "$2" | iprange --print-prefix "route " --print-suffix-nets " via \"$1\";" --print-suffix-ips "/32 via \"$1\";" -
+    get_as_func "$2" | iprange $ipr_verb --print-prefix "route " --print-suffix-nets " via \"$1\";" --print-suffix-ips "/32 via \"$1\";" -
+  fi
+}
+
+ #DIFF FUNCTION
+diff_funk() {
+  if [[ "$DEBUG" == 1 ]]; then
+    patch_file=/tmp/patch_$(echo $1 | awk -F/ '{print $NF}')
+    echo "########### $(date) STEP_3: diff $(echo $1 | awk -F/ '{print $NF}') ###########"
+    diff -u $1 $2 > $patch_file
+	cat $patch_file && patch $1 $patch_file && rm $patch_file
+  else
+    diff -u $1 $2 | patch $1 -
   fi
 }
 
  #INIT FILES
+if [[ "$DEBUG" == 1 ]]; then echo "$(date) STEP_1: add init files ###########"; fi
 WORK_FILES="$BLACKLIST \
             $ROUTE_FORCE_ISP $ROUTE_FORCE_VPN1 $ROUTE_FORCE_VPN2 \
             $ROUTE_BASE_VPN1 $ROUTE_USER_VPN1 \
@@ -55,23 +70,25 @@ for var in $WORK_FILES; do
 done
 
  #WAIT DNS
+if [[ "$DEBUG" == 1 ]]; then echo "$(date) STEP_2: wait dns ###########"; fi
 until ADDRS=$(dig +short google.com @localhost -p 53) && [ -n "$ADDRS" ] > /dev/null 2>&1; do sleep 5; done
 
  #BASE_LIST
-curl -sk $URL0 | sort | diff -u $BLACKLIST - | patch $BLACKLIST -
-ipr_func $VPN1 $BLACKLIST | diff -u $ROUTE_BASE_VPN1 - | patch $ROUTE_BASE_VPN1 -
-sed "s/$VPN1/$VPN2/g" $ROUTE_BASE_VPN1 | diff -u $ROUTE_BASE_VPN2 - | patch $ROUTE_BASE_VPN2 -
+curl -sk $URL0 | sort | diff_funk $BLACKLIST -
+ipr_func $VPN1 $BLACKLIST | diff_funk $ROUTE_BASE_VPN1 -
+sed "s/$VPN1/$VPN2/g" $ROUTE_BASE_VPN1 | diff_funk $ROUTE_BASE_VPN2 -
 
  #BASE_USER_LIST
-ipr_func $VPN1 $VPNTXT | diff -u $ROUTE_USER_VPN1 - | patch $ROUTE_USER_VPN1 -
-sed "s/$VPN1/$VPN2/g" $ROUTE_USER_VPN1 | diff -u $ROUTE_USER_VPN2 - | patch $ROUTE_USER_VPN2 -
+ipr_func $VPN1 $VPNTXT | diff_funk $ROUTE_USER_VPN1 -
+sed "s/$VPN1/$VPN2/g" $ROUTE_USER_VPN1 | diff_funk $ROUTE_USER_VPN2 -
 
  #FORCE_LIST
-ipr_func $ISP $ISPTXT | diff -u $ROUTE_FORCE_ISP - | patch $ROUTE_FORCE_ISP -
-ipr_func $VPN1 $VPN1TXT | diff -u $ROUTE_FORCE_VPN1 - | patch $ROUTE_FORCE_VPN1 -
-ipr_func $VPN2 $VPN2TXT | diff -u $ROUTE_FORCE_VPN2 - | patch $ROUTE_FORCE_VPN2 -
+ipr_func $ISP $ISPTXT | diff_funk $ROUTE_FORCE_ISP -
+ipr_func $VPN1 $VPN1TXT | diff_funk $ROUTE_FORCE_VPN1 -
+ipr_func $VPN2 $VPN2TXT | diff_funk $ROUTE_FORCE_VPN2 -
 
  #RESTART BIRD
+if [[ "$DEBUG" == 1 ]]; then echo "$(date) STEP_4: restart bird ###########"; fi
 if [ "$(cat $MD5_SUM)" != "$(md5sum /opt/etc/bird4*)" ]; then
   md5sum /opt/etc/bird4* > $MD5_SUM
   echo "Restarting bird"
--- Install/one_vpn/add-bird4_routes.sh
+++ Install/one_vpn/add-bird4_routes.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
  #USER VARIABLE
+DEBUG=0
 ISP=ISPINPUT
 VPN1=VPN1INPUT
 HOMEPATH=/opt/root/Bird4Static
@@ -14,6 +15,7 @@ ROUTE_BASE_VPN1=/opt/etc/bird4-base-vpn1.list
 VPN1TXT=$HOMEPATH/lists/user-vpn.list
 ISPTXT=$HOMEPATH/lists/user-isp.list
 MD5_SUM=$HOMEPATH/scripts/sum.md5
+
  #GET AS LIST FUNCTION
 get_as_func() {
   as_list=$(awk '/^AS([0-9]{1,5})/{print $1}' "$1")
@@ -29,14 +31,28 @@ fi
 
  #IPRANGE FUNCTION
 ipr_func() {
+  if [[ "$DEBUG" == 1 ]]; then ipr_verb="-v"; fi
   if [[ $1 =~ ^\([0-9]{1,3}\.\){3}[0-9]{1,3}$ ]]; then
-    get_as_func "$2" | iprange --print-prefix "route " --print-suffix-nets " via $1;" --print-suffix-ips "/32 via $1;" -
+    get_as_func "$2" | iprange $ipr_verb --print-prefix "route " --print-suffix-nets " via $1;" --print-suffix-ips "/32 via $1;" -
   else
-    get_as_func "$2" | iprange --print-prefix "route " --print-suffix-nets " via \"$1\";" --print-suffix-ips "/32 via \"$1\";" -
+    get_as_func "$2" | iprange $ipr_verb --print-prefix "route " --print-suffix-nets " via \"$1\";" --print-suffix-ips "/32 via \"$1\";" -
+  fi
+}
+
+ #DIFF FUNCTION
+diff_funk() {
+  if [[ "$DEBUG" == 1 ]]; then
+    patch_file=/tmp/patch_$(echo $1 | awk -F/ '{print $NF}')
+    echo "########### $(date) STEP_3: diff $(echo $1 | awk -F/ '{print $NF}' ) ###########"
+    diff -u $1 $2 > $patch_file
+	cat $patch_file && patch $1 $patch_file && rm $patch_file
+  else
+    diff -u $1 $2 | patch $1 -
   fi
 }
 
  #INIT FILES
+if [[ "$DEBUG" == 1 ]]; then echo "$(date) STEP_1: add init files ###########"; fi
 WORK_FILES="$BLACKLIST \
             $ROUTE_FORCE_ISP $ROUTE_FORCE_VPN1 \
             $ROUTE_BASE_VPN1 \
@@ -47,17 +63,19 @@ for var in $WORK_FILES; do
 done
 
  #WAIT DNS
+if [[ "$DEBUG" == 1 ]]; then echo "$(date) STEP_2: wait dns ###########"; fi
 until ADDRS=$(dig +short google.com @localhost -p 53) && [ -n "$ADDRS" ] > /dev/null 2>&1; do sleep 5; done
 
  #BASE_LIST
-curl -sk $URL0 | sort | diff -u $BLACKLIST - | patch $BLACKLIST -
-ipr_func $VPN1 $BLACKLIST | diff -u $ROUTE_BASE_VPN1 - | patch $ROUTE_BASE_VPN1 -
+curl -sk $URL0 | sort | diff_funk $BLACKLIST -
+ipr_func $VPN1 $BLACKLIST | diff_funk $ROUTE_BASE_VPN1 -
 
  #FORCE_LIST
-ipr_func $ISP $ISPTXT | diff -u $ROUTE_FORCE_ISP - | patch $ROUTE_FORCE_ISP -
-ipr_func $VPN1 $VPN1TXT | diff -u $ROUTE_FORCE_VPN1 - | patch $ROUTE_FORCE_VPN1 -
+ipr_func $ISP $ISPTXT | diff_funk $ROUTE_FORCE_ISP -
+ipr_func $VPN1 $VPN1TXT | diff_funk $ROUTE_FORCE_VPN1 -
 
  #RESTART BIRD
+if [[ "$DEBUG" == 1 ]]; then echo "$(date) STEP_4:  restart bird ###########"; fi
 if [ "$(cat $MD5_SUM)" != "$(md5sum /opt/etc/bird4*)" ]; then
   md5sum /opt/etc/bird4* > $MD5_SUM
   echo "Restarting bird"

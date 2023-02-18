6cba1c5ea536c296efbd38a56fe9925e9fdd38e3
DennoN-RUS <dennon.ru@gmail.com>
2023-02-18 21:27:19 +0300

    v3.7.0 fix update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ABSOLUTE_FILENAME=`readlink -f "$0"` && HOME_FOLDER=`dirname "$ABSOLUTE_FILENAME"`
-SYSTEM_FOLDER=`echo $HOME_FOLDER | awk -F/opt '{print $1}'`
+SYSTEM_FOLDER=`echo $HOME_FOLDER | awk -F/opt '{print $1}'` && SYSTEM_FOLDER=$SYSTEM_FOLDER/opt
 cd $HOME_FOLDER
 chmod -x *.sh
 $SYSTEM_FOLDER/bin/git status
